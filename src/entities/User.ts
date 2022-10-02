a457cd43e2c4b880fc055a886dbee802f4cd03f0
Programacion315 <jorgeluishuergo88@gmail.com>
2022-10-02 17:57:40 -0500

    feat: app structure

---
+++ src/entities/User.ts
@@ -0,0 +1,23 @@
+import {BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm'
+
+@Entity()
+export class User extends BaseEntity{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstname: string;
+
+    @Column()
+    lastname: string;
+
+    @Column()
+    active: boolean;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
---
+++ src/routes/user.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { createUser } from "../controllers/users.controllers";
+
+const router = Router()
+
+
+
+export default router
